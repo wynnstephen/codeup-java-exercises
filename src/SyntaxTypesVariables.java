69fdbfa1d800dcab0fd74e8bf0fab00a7c08ef05
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-21 16:21:14 -0500

    adds lecture notes

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,12 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+        byte myFirstNumber = 127;
+        byte mySecondNumber = 1;
+        myFirstNumber += mySecondNumber;
+        System.out.println(myFirstNumber + mySecondNumber);
+
+    }
+
+}
