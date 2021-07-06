5ee6c7a32571bd0e7471b5c7cf4d75eb6ffbdffa
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-06 09:01:33 -0500

    adds exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         double pi = 3.14159;
+        sc.useDelimiter("\n");
 
         System.out.println("The value of pi is approximately " + pi);
 
+        System.out.format("The value of pi is approximately %.2f.", pi);
     }
 }
