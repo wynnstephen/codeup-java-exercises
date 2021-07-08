012f51f3a162a3d6dc5eae46eedf123eecb49f50
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-08 11:07:21 -0500

    adds ArrayLecture

---
+++ src/CalculatorWarmUp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Create a command line calculator
+//the user may enter a number
+//the user may enter an operator
+//give the user an option (+, -, x, /)  and use a conditional to know which operation to do
+//don't try to convert the string input into an actual operator
+//the user may enter a second number
+//the program should output a result, then prompt the user to do another calculation (recursion)
+//CalculatorApp.java (for your main method), Calculator.java (for your actual calculator programming), Input.java (for user input)
+
+public class CalculatorWarmUp {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/collection/ArrayLecture.java
@@ -0,0 +1,13 @@
+package collection;
+
+import java.lang.reflect.Array;
+
+import java.util.Arrays;
+
+public class ArrayLecture {
+    public static void main(String[] args) {
+        int[] intArray = new int [] {1, 2, 3};
+
+        System.out.println(Arrays.toString(intArray));
+    }
+}
