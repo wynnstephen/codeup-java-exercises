99319c84e61d38d7016dc141355c97e292e8d26b
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-08 16:55:06 -0500

    adds exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,7 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+    }
+    public static Persons(){
+
+    }
+}
--- src/collection/ArrayLecture.java
+++ src/collection/ArrayLecture.java
@@ -5,9 +5,25 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayLecture {
-    public static void main(String[] args) {
-        int[] intArray = new int [] {1, 2, 3};
 
-        System.out.println(Arrays.toString(intArray));
+    public static void main(String[] args) {
+        System.out.println();
+
     }
+//
+//    //        int[] intArray = new int [] {1, 2, 3};
+//    public static int[] getIntArray() {
+//        return new int[]{1, 2, 3};
+//    }
+//
+//    public static int[] getIntArray(int length) {
+//        int[] intArray = new int[length];
+//
+//        for (int i = 0; i < intArray.length; i++) {
+//            intArray[i] = i * 2;
+//        }
+//        return intArray;
+//    }
+
 }
+
