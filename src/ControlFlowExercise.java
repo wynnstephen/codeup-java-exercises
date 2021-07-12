a5bdefb52f05496a7109b09e80fac18aca12386b
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-12 15:27:03 -0500

    adds finished exercise

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ControlFlowExercise {
@@ -49,7 +50,60 @@ public class ControlFlowExercise {
 //            }
 
         // 3. Table of Powers
+            Scanner scanner = new Scanner(System.in);
+//            while(true) {
+//                System.out.println("What number would you like to go up to?");
+//                int input = scanner.nextInt();
+//
+//                System.out.println("Here is your table!\n");
+//                System.out.println("number | squared | cubed");
+//                System.out.println("------ | ------  | ------");
+//                for (int i = 1; i <= input; i++) {
+//                    int squared = i * i;
+//                    int cubed = i * i * i;
+//                    System.out.println(i + "      | " + squared + "\t\t | " + cubed);
+//                }
+//                System.out.println("Would you like to continue? (yes/no)");
+//                String answer = scanner.next().toLowerCase();
+//                if (answer.equals("yes")){
+//                }else {
+//                    break;
+//                }
+//            }
 
-
+            // 4.
+            while(true) {
+                System.out.println("Please enter the percentage grade you got.");
+                // use byte.
+                byte grade = scanner.nextByte();
+                if (grade >= 100) {
+                    System.out.println("A");
+                } else if (grade >= 88) {
+                    System.out.println("A");
+                } else if (grade >= 87) {
+                    System.out.println("B");
+                } else if (grade >= 80) {
+                    System.out.println("B");
+                } else if (grade >= 79) {
+                    System.out.println("C");
+                } else if (grade >= 67) {
+                    System.out.println("C");
+                }else if ( grade >= 66){
+                    System.out.println("D");
+                } else if (grade >= 60) {
+                    System.out.println("D");
+                }else if ( grade >= 59){
+                    System.out.println("F");
+                }else if (grade >= 0){
+                    System.out.println("F");
+                }
+                System.out.println("would you like to continue? (yes/no)");
+                String prompt = scanner.next().toLowerCase();
+                if (prompt.equals("yes")){
+                    continue;
+                }else {
+                    break;
+                }
+            }
         }
     }
