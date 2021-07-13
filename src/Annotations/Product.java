adb585e77576146f3ad624c8b789765c197fb4cc
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-13 10:31:17 -0500

    adds annotation lecture

---
+++ src/Annotations/Product.java
@@ -0,0 +1,17 @@
+package Annotations;
+
+@NameofAnnotation // syntax // above the thing that you're applying it to
+
+@Entity
+public class Product {
+    // define property name
+  @Persistent
+    protected String name = null;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(name){
+        this.name = name;
+    }
+}
