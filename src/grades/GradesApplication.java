af2f0a09a6785a76706582c274c7b0bba7c3c905
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-20 21:29:14 -0500

    adds grades

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,10 +1,124 @@
-//package grades;
-//
-//import java.util.HashMap;
-//
-//public class GradesApplication {
-//    public static void main(String[] args) {
-//
-//    }
-//    HashMap<String, Student> students= new HashMap<>();
-//}
+package grades;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class GradesApplication {
+    public static HashMap<String, Student> students = new HashMap<>();
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        createStudents();
+
+        home();
+
+    }
+    public static void createStudents(){
+        Student student1 = new Student("Matthew");
+        student1.addGrade(100);
+        student1.addGrade(95);
+        student1.addGrade(90);
+        Student student2 = new Student("Andrew");
+        student2.addGrade(80);
+        student2.addGrade(90);
+        student2.addGrade(75);
+        Student student3 = new Student("Jada");
+        student3.addGrade(65);
+        student3.addGrade(50);
+        student3.addGrade(85);
+        Student student4 = new Student("Jankins");
+        student4.addGrade(95);
+        student4.addGrade(80);
+        student4.addGrade(100);
+
+
+        students.put("Stephen15", student1);
+        students.put("asdf", student2);
+        students.put("asdff", student3);
+        students.put("asdfff", student4);
+    }
+
+    public static void homeMessage() {
+        System.out.println("Welcome.");
+        System.out.println("Here is a list of current students:\n");
+        for (String username : students.keySet()) {
+            System.out.printf(" |%s| ", username);
+        }
+        System.out.println("\n\nOptions:");
+        System.out.println("0 - Exit");
+        System.out.println("1 - View all grades.");
+        System.out.println("2 - View Specific Students");
+        System.out.print("> ");
+
+    }
+    public static void showAllGrades(){
+        for(Student student : students.values()){
+            System.out.printf("\nStudent: " + student.getName() + "\nGrades: " + student.getGrades() + "\nAverage: %.2f\n", student.getGradeAverage());
+
+        }
+    }
+    public static void chooseOption(){
+        String choice = sc.next();
+        if (choice.equalsIgnoreCase("0")){
+            return;
+        }
+        if(choice.equalsIgnoreCase("1")){
+            showAllGrades();
+        }
+        if(choice.equalsIgnoreCase("2")){
+            displayStudent(chooseStudent());
+        }
+
+
+    }
+
+    public static String chooseStudent() {
+        System.out.println("What student would you like to see more information on?");
+        System.out.print(">");
+        String userInput = sc.next();
+        for (String username : students.keySet()) {
+            if (userInput.equalsIgnoreCase(username)) {
+                return username;
+            }
+
+        }
+        System.out.println("Couldn't find username. Try again.");
+        return chooseStudent();
+    }
+    public static void displayStudent(String student){
+
+        Student thisStudent = students.get(student);
+        System.out.printf("Name: %s \nGithub Username: %s \nCurrent Average: %.2f \nList Of Grades: %s\n", thisStudent.getName(), student, thisStudent.getGradeAverage(), thisStudent.getGrades());
+    }
+
+    public static boolean continueCheck(){
+        System.out.println("\nWould you like to continue?");
+        System.out.printf("Enter y or n: ");
+        String input = sc.next();
+        if (input.equalsIgnoreCase("y")){
+            return true;
+        }
+        else if (input.equalsIgnoreCase("n")){
+            System.out.println("\nGoodbye.");
+            return false;
+
+        } else {
+            System.out.println("Not y or n, try again:");
+            return continueCheck();
+        }
+    }
+
+    public static void home() {
+
+        homeMessage();
+
+        chooseOption();
+
+
+        if(continueCheck()){
+            home();
+        }
+
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,17 +1,51 @@
-//package grades;
-//
-//import java.util.ArrayList;
-//
-////public class Student {
-////    public static void main(String[] args) {
-////        Student me = new Student("Stephen");
-////        me.addGrade(80);
-////        me.addGrade(85);
-////    }
-////
-////    public Student(String name) {
-////        this.name = name;
-////        grades = new ArrayList<>();
-////    }
-////
-////}
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    public static void main(String[] args) {
+        Student me = new Student("Matthew");
+        me.addGrade(100);
+        me.addGrade(100);
+        me.addGrade(100);
+        me.addGrade(95.50);
+        me.addGrade(75);
+        me.addGrade(80);
+
+
+        System.out.println(me.getGrades());
+        System.out.println(me.getGradeAverage());
+
+    }
+    private String name;
+    private ArrayList<Double> grades;
+
+    public Student(String name) {
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Double> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(double grade) {
+        this.grades.add(grade);
+    }
+    public double getGradeAverage(){
+        double sum = 0;
+        for (double grade : this.grades){
+            sum += grade;
+        }
+
+        return sum/(this.grades.size());
+    }
+}
