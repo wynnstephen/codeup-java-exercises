3b0f2b4352f4ca69614175aa665377e8af6b45ad
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-24 21:43:50 -0500

    adds shapes

---
+++ src/shapes/Square.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class Square extends Quadrilateral{
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public void setLength(double side) {
+
+        this.length = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+
+        this.width = side;
+
+    }
+}
