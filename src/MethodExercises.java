2a83867d5ae4e02295567906c8b939b21722802e
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-25 21:51:18 -0500

    adds MethodExercises

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -35,15 +35,74 @@
 //    }
 //
 //    // 2.
-//    public  static  int getINteger(int min, int max, Scanner scanner) {
-//        int scanINput = scanner.nextInt();
+//        public static int getInteger(int min, int max){
 //
-//        if (scanInput < min || scanInput > max) {
-//            System.out.println("error, num not an int or within range.");
-//            System.out.println("enter a num 1 thru 10");
-//            return getInteger(min, max, scanner);
-//        }else {
-//            return scanInput;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n1 = scanner.nextInt();
+//
+//        if(n1 < min){
+//            System.out.print("Please re-enter a number between 1 and 10: ");
+//            getInteger(1, 10);
+//        } else if(n1 > max){
+//            System.out.print("Please re-enter a number between 1 and 10: ");
+//            getInteger(1, 10);
+//        } else {
+//            System.out.println(n1 + " is a valid number!");
+//        }
+//        return n1;
+//    }
+//
+////    3.
+//    public static void Factorial(int min, int max){
+//
+//        while (true) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int input = scanner.nextInt();
+//
+//        if(input < min){
+//            System.out.print("Please re-enter a number between 1 and 10: ");
+//            Factorial(1, 10);
+//        } else if(input > max){
+//            System.out.print("Please re-enter a number between 1 and 10: ");
+//            Factorial(1, 10);
+//        } else {
+//
+//            long fact = 1;
+//
+//            for (int i = 1; i <= input; i++) {
+//                fact = fact * i;
+//                System.out.println(i + "! = " + fact);
+//            }
+//
+//                System.out.print("Would you like to continue? (y/n): ");
+//
+//                String prompt = scanner.next().toLowerCase();
+//
+//                if (prompt.equals("y")) {
+//                    System.out.print("Enter a number between 1 and 10: ");
+//                    continue;
+//                } else if (prompt.equals("n")) {
+//                    break;
+//                }
+//
+//            }
+//            break;
 //        }
 //    }
+//
+//
+////
+//    public static int roll(int diceSides){
+//
+//        Random rand = new Random();
+//        int sides = rand.nextInt(diceSides);
+//        sides = sides + 1;
+//
+//        return sides;
+//    }
+//
+//
 //}
